949b5ff3838617f10b3d0ec48023c30e48c9f0a9
gunny091 <ryuee091119@naver.com>
2022-04-16 13:13:16 +0900

    message->messageCreate, add exec,setstatus,logout command

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Client, Intents, Discord } = require("discord.js");
+const { Client, Intents } = require("discord.js");
 const fs = require("fs");
-const { token } = require("./config.json");
+const { token, ownerId } = require("./config.json");
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -23,7 +23,10 @@ client.on("ready", () => {
   client.user.setActivity("테스트");
 });
 
-client.on("message", msg => {
+client.on("messageCreate", msg => {
+  if (msg.author.bot) return;
+  if (msg.author.id === client.user.id) return;
+
   log(
     "Message Send",
     `"${msg.content}" @${msg.author.tag}(${msg.author.id}) .${msg.guild.name}(${msg.guild.id}) #${msg.channel.name}(${msg.channel.id}) *${msg.id}`
@@ -33,6 +36,25 @@ client.on("message", msg => {
     msg.channel.send(`${msg.author} pong`).then(msg => {
       console.log(`Sent massage: "${msg.content}" to ${msg.channel}`);
     });
+  }
+  if (msg.author.id === ownerId) {
+    if (msg.content.length > 7 && msg.content.substring(0, 6) === "t/exec") {
+      try {
+        eval(msg.content.substring(7));
+        msg.channel.send("complete");
+      } catch {
+        msg.channel.send("error");
+      }
+    }
+    if (msg.content === "t/logout") {
+      process.exit(2);
+    }
+    if (
+      msg.content.length > 12 &&
+      msg.content.substring(0, 11) === "t/setstatus"
+    ) {
+      client.user.setActivity(msg.content.substring(12));
+    }
   }
 });
 client.on("messageUpdate", (oldMsg, newMsg) => {
