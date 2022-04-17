f890689cfb3e9359976bdb6a157666565c62c9ee
gunny091 <ryuee091119@naver.com>
2022-04-17 11:32:10 +0900

    버그 픽숴

--- src/sendMessage.js
+++ src/sendMessage.js
@@ -5,7 +5,7 @@ function sendMessage(channel, content) {
   channel.send(content).then(m => {
     logCF(
       "Message Send",
-      `"${content}" @테스트봇#8352(964673407868350474) .${channel.guild.name}(${channel.guild.id}) #${channel.name}(${channel.id}) *${m.id}`,
+      `"${content}" @${client.user.tag}(${client.user.id}) .${channel.guild.name}(${channel.guild.id}) #${channel.name}(${channel.id}) *${m.id}`,
       client
     );
   });
